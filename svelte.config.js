58d454e7439a4d0d8b03f63ffc09ecaceda867e0
irgawev <irgawevrasul@gmail.com>
2024-06-10 00:20:00 +0500

    configure vercel plugin

--- svelte.config.js
+++ svelte.config.js
@@ -8,16 +8,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		images: {
-			sizes: [640, 828, 1200, 1920, 3840],
-			formats: ['image/avif', 'image/webp'],
-			minimumCacheTTL: 300,
-			domains: ['svelte-kit.vercel.app'],
-		}
+		adapter: adapter({
+			// see below for options that can be set here
+		})
 	}
 };
 
